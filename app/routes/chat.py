d338fb7b1147fcf6f6a87d5c7c945f3331214b2c
Anson <awong30@stuy.edu>
2023-06-05 16:11:43 -0400

    individual chat started- renders template, answers prompt

---
+++ app/routes/chat.py
@@ -0,0 +1,33 @@
+from flask import render_template, request, redirect, session
+from flask import current_app as app
+import json 
+from urllib.request import urlopen
+import os
+import openai
+import sqlite3
+
+with open('../keys/openai.txt') as key:
+    key = key.read()
+    openai.api_key = key
+
+def chat():
+    return render_template('chat.html')
+
+def ans(prompt):
+    return(openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a mean assistant. Be very snarky. "},
+        {"role": "user", "content": prompt},
+    ]
+    )['choices'][0]['message']['content'])
+
+
+def history():
+    #should return list of all past msgs from user in session
+    c = app.config['conn'].cursor()
+    c.execute('SELECT id FROM users WHERE username = ?', (session['user'],))
+    userid = c.fetchone()
+    #this selects all msgs would need only from individual chat
+    c.execute('SELECT message_content FROM messages WHERE user_id = ?', (userid))
+    return c.fetchall()
