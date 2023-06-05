2840a25d87ed07c75f06f0734d368fc74db96a2c
Dyredhead <danielyentin@gmail.com>
2023-06-05 10:58:15 -0400

    new room functionality added

--- app/routes/home.py
+++ app/routes/home.py
@@ -6,8 +6,14 @@ def home_get():
     if "user_id" in session:
         db = sqlite3.connect('database.db')
         c = db.cursor()
-        c.execute("SELECT * FROM room_users WHERE user_id=?", (session["user_id"],))
-        rooms = c.fetchall()
+        c.execute("SELECT room_id FROM room_users WHERE user_id = ?", (session["user_id"],))
+        room_ids = c.fetchall()
+        print(room_ids)
+        rooms = []
+        for room_id in room_ids:
+            c.execute("SELECT * FROM rooms WHERE id = ?", (room_id[0],))
+            rooms.append(c.fetchone())
+
         db.commit()
         db.close()
         print(rooms)
--- app/routes/new_room.py
+++ app/routes/new_room.py
@@ -10,12 +10,14 @@ def new_room_get():
 
 def new_room_post():
     name = request.form["name"]
-    
+    print(name)
     db = sqlite3.connect('database.db')
     c = db.cursor()
-    c.execute('INSERT INTO rooms (name, owner_id) VALUES (?, ?)', (name, session["user_id"]))
-    c.execute("SELECT id FROM rooms WHERE owner_id = ?", (session["user_id"],))
+    
+    c.execute("SELECT count(id) FROM rooms")
     room_id = c.fetchone()[0]
+    print(f"room_id: {room_id}")
+    c.execute("INSERT INTO rooms (id, name, owner_id) VALUES (?, ?, ?)", (room_id, name, session["user_id"]))
     c.execute("INSERT INTO room_users (room_id, user_id) VALUES (?, ?)", (room_id, session["user_id"]))
     db.commit()
     db.close()
