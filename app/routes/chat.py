16607172ebadc472511a8c2d1ef5a1d0493023b7
Anson <awong30@stuy.edu>
2023-06-05 16:43:55 -0400

    start insert

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,10 +1,8 @@
 from flask import render_template, request, redirect, session
 from flask import current_app as app
-import json 
+import json, os, openai, sqlite3, datetime
 from urllib.request import urlopen
-import os
-import openai
-import sqlite3
+
 
 with open('keys/openai.txt') as key:
     key = key.read()
@@ -16,9 +14,19 @@ def chat_get():
     else:
         # Render index page by default
         return render_template('index.html')
-
+'''   
+def add_msg(msg):
+    #room 0 for chat as placeholder
+    c = app.config['conn'].cursor()
+    c.execute("SELECT count(id) FROM messages")
+    msg_id = c.fetchone()[0]
+    c.execute("INSERT INTO messages (id, room_id, user_id, message_content, timestamp) VALUES (?, ?, ?, ?, ?)", (msg_id, 0, session["user_id"], msg, datetime.now()))
+    c.execute('SELECT id FROM users WHERE username = ?', (session['user'],))
+'''
+    
 def ans(prompt):
     #need to insert
+    #add_msg(prompt)
     return(openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
@@ -26,9 +34,6 @@ def ans(prompt):
         {"role": "user", "content": prompt},
     ]
     )['choices'][0]['message']['content'])
-
-
-
 
 def history():
     #should return list of all past msgs from user in session
